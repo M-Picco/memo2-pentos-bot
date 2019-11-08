d414e493e43abb10058d05c9eb3604911e6cec46
you@example.com <you@example.com>
2019-11-08 11:44:15 -0300

    Example code cleanup

--- spec/bot_client_spec.rb
+++ spec/bot_client_spec.rb
@@ -8,8 +8,8 @@ require File.dirname(__FILE__) + '/../app/bot_client'
 def stub_get_updates(token, message_text)
   body = { "ok": true, "result": [{ "update_id": 693_981_718,
                                     "message": { "message_id": 11,
-                                                 "from": { "id": 141_733_544, "is_bot": false, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "language_code": 'en' },
-                                                 "chat": { "id": 141_733_544, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "type": 'private' },
+                                                 "from": { "id": 141_733_544, "is_bot": false, "first_name": 'Cliente', "last_name": 'Hambriento', "username": 'chambriento', "language_code": 'en' },
+                                                 "chat": { "id": 141_733_544, "first_name": 'Cliente', "last_name": 'Hambriento', "username": 'chambriento', "type": 'private' },
                                                  "date": 1_557_782_998, "text": message_text,
                                                  "entities": [{ "offset": 0, "length": 6, "type": 'bot_command' }] } }] }
 
@@ -21,7 +21,7 @@ def stub_send_message(token, message_text)
   body = { "ok": true,
            "result": { "message_id": 12,
                        "from": { "id": 715_612_264, "is_bot": true, "first_name": 'fiuba-memo2-prueba', "username": 'fiuba_memo2_bot' },
-                       "chat": { "id": 141_733_544, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "type": 'private' },
+                       "chat": { "id": 141_733_544, "first_name": 'Cliente', "last_name": 'Hambriento', "username": 'chambriento', "type": 'private' },
                        "date": 1_557_782_999, "text": message_text } }
 
   stub_request(:post, "https://api.telegram.org/bot#{token}/sendMessage")
@@ -29,39 +29,4 @@ def stub_send_message(token, message_text)
       body: { 'chat_id' => '141733544', 'text' => message_text }
     )
     .to_return(status: 200, body: body.to_json, headers: {})
-end
-
-describe 'BotClient' do
-  it 'should get a /start message and respond with Hola' do
-    token = 'fake_token'
-
-    stub_get_updates(token, '/start')
-    stub_send_message(token, 'Hola, Emilio')
-
-    app = BotClient.new(token)
-
-    app.run_once
-  end
-
-  it 'should get a /stop message and respond with Chau' do
-    token = 'fake_token'
-
-    stub_get_updates(token, '/stop')
-    stub_send_message(token, 'Chau, egutter')
-
-    app = BotClient.new(token)
-
-    app.run_once
-  end
-
-  it 'should get an unknown message message and respond with Do not understand' do
-    token = 'fake_token'
-
-    stub_get_updates(token, '/unknown')
-    stub_send_message(token, 'Mmm no entendi, Me repetis la pregunta?')
-
-    app = BotClient.new(token)
-
-    app.run_once
-  end
 end
