383104ba85cb949c1db1317f80ecd56b19178bec
M-Picco <martinalejandropicco1996@gmail.com>
2019-11-09 15:04:55 -0300

    Use ApiErrorMapper at ApiClient

--- app/api_client.rb
+++ app/api_client.rb
@@ -1,13 +1,7 @@
 class ApiClient
-  ERRORS = {
-    'invalid_username' => 'usuario invalido',
-    'invalid_address' => 'direccion invalida',
-    'invalid_phone' => 'telefono invalido',
-    'server_error' => 'error del servidor, espere y vuelva a intentarlo'
-  }.freeze
-
   def initialize(api_base_url)
     @api_base_url = api_base_url
+    @error_mapper = ApiErrorMapper.new
   end
 
   def register(username, address, phone)
@@ -20,17 +14,13 @@ class ApiClient
     response = Faraday.post(endpoint('/client'), params.to_json, 'Content-Type' => 'application/json')
     body = JSON.parse(response.body)
 
-    raise parse_error(body['error']) if response.status == 403
-    raise parse_error('server_error') if response.status == 500
+    raise @error_mapper.map(body['error']) if response.status == 403
+    raise @error_mapper.map('server_error') if response.status == 500
 
     body['client_id']
   end
 
   private
-
-  def parse_error(error_message)
-    ERRORS[error_message]
-  end
 
   def endpoint(route)
     "#{@api_base_url}#{route}"
